021bb4cfade0b17972e994faecfebba418c8eae9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 18:08:52 +0000

    1.18API Support.

--- src/main/java/me/loving11ish/poscommands/PosCommands.java
+++ src/main/java/me/loving11ish/poscommands/PosCommands.java
@@ -26,7 +26,7 @@ public final class PosCommands extends JavaPlugin {
         //Server version compatibility check
         if (!(Bukkit.getServer().getVersion().contains("1.13")||Bukkit.getServer().getVersion().contains("1.14")
                 ||Bukkit.getServer().getVersion().contains("1.15")||Bukkit.getServer().getVersion().contains("1.16")
-                ||Bukkit.getServer().getVersion().contains("1.17"))){
+                ||Bukkit.getServer().getVersion().contains("1.17")||Bukkit.getServer().getVersion().contains("1.18"))){
             logger.warning(ChatColor.RED + "-------------------------------------------");
             logger.warning(ChatColor.RED + "PosCommands - This plugin is only supported on the Minecraft versions listed below:");
             logger.warning(ChatColor.RED + "PosCommands - 1.13.x");
@@ -34,6 +34,7 @@ public final class PosCommands extends JavaPlugin {
             logger.warning(ChatColor.RED + "PosCommands - 1.15.x");
             logger.warning(ChatColor.RED + "PosCommands - 1.16.x");
             logger.warning(ChatColor.RED + "PosCommands - 1.17.x");
+            logger.warning(ChatColor.RED + "PosCommands - 1.18.x");
             logger.warning(ChatColor.RED + "PosCommands - Is now disabling!");
             logger.warning(ChatColor.RED + "-------------------------------------------");
             Bukkit.getPluginManager().disablePlugin(plugin);
